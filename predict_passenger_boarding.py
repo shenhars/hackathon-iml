a34bc2f83df736880b1f5117761eba7118f789a7
netahoch <neta.hochauser@mail.huji.ac.il>
2024-07-03 22:46:12 +0300

    ijo

--- predict_passenger_boarding.py
+++ predict_passenger_boarding.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from typing import Optional
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import *
